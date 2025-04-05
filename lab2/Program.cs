e170f367333486088a0a49b9ef19cf684afc02de
Błażej Szulik <b.szulik@tuta.io>
2025-04-05 13:13:35 +0200

    init commit

---
+++ lab2/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Net;
+
+namespace lab2
+{
+    class Program
+    {
+        interface IPaczka {
+            void Przygotuj();
+        }
+
+        interface IFabrykaPaczek {
+            IPaczka UtworzPaczke();
+        }
+
+        class MalaPaczka : IPaczka {
+            public void Przygotuj() {
+                Console.WriteLine("Przygotowano małą paczkę.");
+            }
+        }
+
+        class DuzaPaczka : IPaczka {
+            public void Przygotuj() {
+                Console.WriteLine("Przygotowano dużą paczkę");
+            }
+        }
+
+        class FabrykaMalychPaczek : IFabrykaPaczek {
+            public IPaczka UtworzPaczke() {
+                return new MalaPaczka();
+            }
+        }
+
+        class FabrykaDuzychPaczek : IFabrykaPaczek {
+            public IPaczka UtworzPaczke() {
+                return new DuzaPaczka();
+            }
+        }
+
+        class ZarzadzanieProdukcja {
+            private IFabrykaPaczek fabrykaPaczek;
+            private static ZarzadzanieProdukcja _instance;
+            private ZarzadzanieProdukcja() { }
+            public static ZarzadzanieProdukcja getInstance() {
+                if (_instance == null) {
+                    _instance = new ZarzadzanieProdukcja();
+                }
+                return _instance;
+            }
+
+            public void produkujMalePaczki() {
+                fabrykaPaczek = new FabrykaMalychPaczek();
+            }
+            public void produkujDuzePaczki() {
+                fabrykaPaczek = new FabrykaDuzychPaczek();
+            }
+            public void wyprodukujPaczke() {
+                fabrykaPaczek.UtworzPaczke().Przygotuj();
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            ZarzadzanieProdukcja produkcja = ZarzadzanieProdukcja.getInstance();
+            ZarzadzanieProdukcja produkcja2 = ZarzadzanieProdukcja.getInstance();
+            if (produkcja != produkcja2) {
+                Console.WriteLine("singleton nie dziala");
+                return;
+            }
+            produkcja.produkujMalePaczki();
+            produkcja.wyprodukujPaczke();
+        }
+    }
+}
---
+++ lab2_2/Program.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace lab2_2
+{
+    class Program
+    {
+        public enum Rozmiar {
+            mala, 
+            duza
+        }
+        public enum Kurier {
+            DHL,
+            UPS
+        }
+        interface IPaczka {
+            public void Spakuj();
+        }
+
+        interface IKurier {
+            public void Dostarcz();
+        }
+
+        interface IFabrykaLogistyki {
+            IPaczka utworzPaczke(Rozmiar rozmiar);
+            IKurier utworzKuriera(Kurier kurier);
+        }
+
+        class MalaPaczka : IPaczka {
+            public void Spakuj() {
+                Console.WriteLine("Spakowano małą paczkę");
+            }
+        }
+
+        class DuzaPaczka : IPaczka {
+            public void Spakuj() {
+                Console.WriteLine("Spakowano dużą paczkę");
+            }
+        }
+
+        class DHLKurier : IKurier {
+            public void Dostarcz() {
+                Console.WriteLine("Dostarczono przez kuriera DHL");
+            }
+        }
+
+        class UPSKurier : IKurier {
+            public void Dostarcz() {
+                Console.WriteLine("Dostarczono przez kuriera UPS");
+            }
+        }
+
+        class FabrykaLogistykiPolska : IFabrykaLogistyki
+        {
+            public IKurier utworzKuriera(Kurier kurier)
+            {
+                switch (kurier) {
+                    case Kurier.DHL:
+                        return new DHLKurier();
+                    case Kurier.UPS:
+                        return new UPSKurier();
+                    default:
+                        return null;
+                }
+            }
+
+            public IPaczka utworzPaczke(Rozmiar rozmiar)
+            {
+                switch (rozmiar) {
+                    case Rozmiar.mala:
+                        return new MalaPaczka();
+                    case Rozmiar.duza:
+                        return new DuzaPaczka();
+                    default:
+                        return null;
+                }
+            }
+        }
+
+            class FabrykaLogistykiUSA : IFabrykaLogistyki
+            {
+                public IKurier utworzKuriera(Kurier kurier)
+                {
+                    switch (kurier) {
+                        case Kurier.DHL:
+                            return new DHLKurier();
+                        case Kurier.UPS:
+                            return new UPSKurier();
+                        default:
+                            return null;
+                    }
+                }
+
+            public IPaczka utworzPaczke(Rozmiar rozmiar)
+                {
+                    switch (rozmiar) {
+                        case Rozmiar.mala:
+                            return new MalaPaczka();
+                        case Rozmiar.duza:
+                            return new DuzaPaczka();
+                        default:
+                            return null;
+                    }
+                }
+            }
+
+            class ZarzadzaniePrzesylkami {
+            private IFabrykaLogistyki fabrykaLogistyki;
+            private static ZarzadzaniePrzesylkami _instance;
+            private ZarzadzaniePrzesylkami() {}
+            public static ZarzadzaniePrzesylkami Instance {
+                get {
+                    if (_instance == null) {
+                        _instance = new ZarzadzaniePrzesylkami();
+                    }
+                    return _instance;
+                }
+            }
+            public enum Lokalizacja {
+                Polska,
+                USA
+            }
+
+            public void PrzyjmijZamowienie(Lokalizacja lokalizacja, Rozmiar rozmiar, Kurier kurier) {
+                switch (lokalizacja) {
+                    case Lokalizacja.Polska:
+                        fabrykaLogistyki = new FabrykaLogistykiPolska();
+                        break;
+                    case Lokalizacja.USA:
+                        fabrykaLogistyki = new FabrykaLogistykiUSA();
+                        break;
+                    default:
+                        throw new InvalidEnumArgumentException("Nieobsługiwana lokalizacja");
+                }
+
+                IPaczka p = fabrykaLogistyki.utworzPaczke(rozmiar);
+                IKurier k = fabrykaLogistyki.utworzKuriera(kurier);
+                p.Spakuj();
+                k.Dostarcz();
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            ZarzadzaniePrzesylkami.Instance.PrzyjmijZamowienie(ZarzadzaniePrzesylkami.Lokalizacja.Polska, Rozmiar.duza, Kurier.DHL);
+            ZarzadzaniePrzesylkami.Instance.PrzyjmijZamowienie(ZarzadzaniePrzesylkami.Lokalizacja.USA, Rozmiar.mala, Kurier.UPS);
+        }
+    }
+}
